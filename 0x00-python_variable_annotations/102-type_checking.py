0acbb54b1b06b530e9eb99061312cd7f95b14a93
wuhibe <wuhibetamire710@gmail.com>
2022-04-14 23:46:16 +0300

    modify input

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,18 +3,17 @@
 import typing
 
 
-def zoom_array(lst: typing.List, factor: typing.Union[float, int] =
-               2) -> typing.List:
+def zoom_array(lst: typing.Tuple, factor: int = 2) -> typing.List:
     ''' function to zoom array '''
     zoomed_in: typing.List = [
         item for item in lst
-        for i in range(int(factor))
+        for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array: typing.Tuple = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, int(3.0))

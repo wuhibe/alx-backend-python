5826340bed839d46721e3ee80847b58ab6f39d26
wuhibe <wuhibetamire710@gmail.com>
2022-04-14 22:30:41 +0300

    task 10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,7 @@ import typing
 
 
 def safe_first_element(lst: typing.Sequence[typing.Any]) -> typing.Union[
-        typing.Any, NoneType]:
+        typing.Any, None]:
     ''' function with correct duck-typed annotations '''
     if lst:
         return lst[0]

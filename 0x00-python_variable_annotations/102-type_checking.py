a1a4c396a83773b4d419179a7364b55872a7ded4
wuhibe <wuhibetamire710@gmail.com>
2022-04-14 23:32:43 +0300

    task 12

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+''' module for task 12 '''
+import typing
+
+
+def zoom_array(lst: typing.List, factor: typing.Union[float, int] =
+               2) -> typing.List:
+    ''' function to zoom array '''
+    zoomed_in: typing.List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91, 13, 45, 23, 76, 23, 75]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)

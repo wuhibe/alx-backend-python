72e3183a20499acb929937c4951e7d10907e7730
wuhibe <wuhibetamire710@gmail.com>
2022-04-14 21:16:07 +0300

    explicit typing

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ''' module for task 4 '''
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = 'Holberton'
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'

42b3b2f4b5e8d0f3b8581c7e7fe7b6a45f5d2aab
wuhibe <wuhibetamire710@gmail.com>
2022-04-19 22:32:15 +0300

    typing and loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,12 @@
 ''' module for task 0 '''
 import asyncio
 import random
+import typing
 
 
-async def async_generator() -> float:
+async def async_generator() -> typing.Generator[float, None, None]:
     ''' function that asynchronously waits 1 second,
     then yield a random number between 0 and 10.'''
-    await asyncio.sleep(1)
-    yield random.uniform(0, 10)
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)

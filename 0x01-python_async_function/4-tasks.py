393b16349d5b18bdee171f48e232be7d6480529b
wuhibe <wuhibetamire710@gmail.com>
2022-04-19 22:14:49 +0300

    pycodestyle

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
-    '''function that calls task_wait_random n times with the specified max_delay'''
+    '''function that calls task_wait_random n times
+     with the specified max_delay'''
     tasks: typing.List[float] = []
     lst: typing.List[float] = []
     while n > 0:

2aa0c4478926aadfe1e8b1e161ff35fb746fe9a7
wuhibe <wuhibetamire710@gmail.com>
2022-05-08 15:47:56 +0300

    first commit

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module to test utils """
+import unittest
+from parameterized import parameterized
+import utils
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Class for access nested map class """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, result):
+        """ test for access nested map method """
+        self.assertEqual(utils.access_nested_map(nested_map, path), result)

a9c726d3f94dc5802de1b65e25cdf158cfc543dd
wuhibe <wuhibetamire710@gmail.com>
2022-05-10 01:07:15 +0300

    task 9 fail

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -60,7 +60,6 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(rtrn, test_return)
 
 
-
 @parameterized_class(
     ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
     TEST_PAYLOAD
@@ -72,6 +71,30 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         """ It is part of the unittest.TestCase API
         method to return example payloads found in the fixtures """
         cls.get_patcher = patch('requests.get', side_effect=HTTPError)
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock):
+        """ to unit-test GithubOrgClient.public_repos """
+        mock.return_value = [{"name": "holberton"}]
+        with patch.object(GithubOrgClient,
+                          "_public_repos_url",
+                          new_callable=PropertyMock,
+                          return_value="https://api.github.com/") as mock_pub:
+            test_client = GithubOrgClient("hoberton")
+            test_return = test_client.public_repos()
+            self.assertEqual(test_return, ["holberton"])
+            mock.assert_called_once
+            mock_pub.assert_called_once
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ])
+    def test_has_license(self, repo, license_key, rtrn):
+        """ to unit-test GithubOrgClient.has_license """
+        test_client = GithubOrgClient("holberton")
+        test_return = test_client.has_license(repo, license_key)
+        self.assertEqual(rtrn, test_return)
 
     @classmethod
     def tearDownClass(cls):
